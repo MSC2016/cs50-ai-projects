453e7ae12efa3ffdea96aab668db32a3db9d903f
MSC2016 <17535858+MSC2016@users.noreply.github.com>
2025-06-08 18:22:20 +0100

    missing the best part:   tictactoe.py

--- project0/tictactoe/tictactoe.py
+++ project0/tictactoe/tictactoe.py
@@ -2,7 +2,7 @@
 Tic Tac Toe Player
 """
 
-import math
+import math, copy
 
 X = "X"
 O = "O"
@@ -23,7 +23,7 @@ def player(board):
     Returns player who has the next turn on a board.
     """
 
-    #initializa variables to count the amount of moves each player has done
+    #initializa variables to count the amount of moves for each player
     x_count = 0
     o_count = 0
 
@@ -43,14 +43,31 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    # create the variable that holds the return value
+    actions = set()
+
+    # iterate trough all cells and add them to the possible actions
+    # if they are EMPTY
+    for x in range(3):
+        for y in range(3):
+            if board[x][y] == EMPTY:
+                actions.add(x,y)
+
+    return actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    # create a deepcopy of the board
+    updated_board = copy.deepcopy(board)
+
+    # get the next player to move and change the cell in the copy
+    updated_board[action[0]][action[1]] = player(board)
+
+    # return the updated board
+    return updated_board
 
 
 def winner(board):
@@ -58,30 +75,30 @@ def winner(board):
     Returns the winner of the game, if there is one.
     """
 
-    winner = None
+    has_a_line = None
     # iterate trough x,y in the board
     for x in range(3):
         for y in range(3):
 
             # check first diagonal
             if x == 1 and y == 1:
-                winner = board[x][y] if board[x-1][y-1] == board[x][y] == board[x+1][y+1] else None
+                has_a_line = board[x][y] if board[x-1][y-1] == board[x][y] == board[x+1][y+1] else None
 
                 # check second diagonal if no winner was found
-                if winner == None:
-                    winner = board[x][y] if board[x+1][y-1] == board[x][y] == board[x-1][y+1] else None
+                if has_a_line == None:
+                    has_a_line = board[x][y] if board[x+1][y-1] == board[x][y] == board[x-1][y+1] else None
 
             # check horizontal lines
-            if x == 1 and winner == None:
-                winner = board[x][y] if board[x-1][y] == board[x][y] == board[x+1][y] else None
+            if x == 1 and has_a_line == None:
+                has_a_line = board[x][y] if board[x-1][y] == board[x][y] == board[x+1][y] else None
 
             # check vertical lines
-            if y == 1 and winner == None:
-                winner = board[x][y] if board[x][y-1] == board[x][y] == board[x][y+1] else None
+            if y == 1 and has_a_line == None:
+                has_a_line = board[x][y] if board[x][y-1] == board[x][y] == board[x][y+1] else None
 
             # return a winner if one is found
-            if winner:
-                return winner
+            if has_a_line:
+                return has_a_line
             
             # previous if statements checked if a winner was found before to avoid
             # overwritting a previously found winning line, with a line of EMPTY cells 
@@ -100,9 +117,9 @@ def terminal(board):
 
     #iterate trough all positions in all lines and 
     # return false if there is still empty positions
-    for line in board:
-        for position in line:
-            if position == EMPTY:
+    for row in board:
+        for cell in row:
+            if cell == EMPTY:
                 return False
             
     #If there is no winner, and all positions are occupied return True(Game Over)
@@ -113,7 +130,16 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    # return 1 if X won
+    if winner(board) == X:
+        return 1
+    
+    # return -1 if O won
+    if winner(board) == O:
+        return -1
+    
+    # return 0 if none of the previous conditions was met
+    return 0
 
 
 def minimax(board):
