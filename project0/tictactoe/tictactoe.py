79cc288b0319d71d46675396227a6d2b0e80e809
MSC2016 <17535858+MSC2016@users.noreply.github.com>
2025-06-08 18:06:44 +0100

    modified:   tictactoe.py

--- project0/tictactoe/tictactoe.py
+++ project0/tictactoe/tictactoe.py
@@ -23,19 +23,19 @@ def player(board):
     Returns player who has the next turn on a board.
     """
 
-    #initializa variabled to count the amount of moves each player has done
+    #initializa variables to count the amount of moves each player has done
     x_count = 0
     o_count = 0
 
     # iterate trough the board and count the amount of X and O moves
-    for x in range(len(board)):
-        for y in range(len(board)):
+    for x in range(3):
+        for y in range(3):
             if board[x][y] == X:
                 x_count += 1
             if board[x][y] == O:
                 o_count += 1
     
-    print(" It's ", O if x_count > o_count else X, "'s turn to play")
+    # return O if X has done more moves
     return O if x_count > o_count else X
 
 
@@ -57,21 +57,36 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    board = [[X, O, O],
-             [EMPTY, X, O],
-             [EMPTY, EMPTY, X]]
-    
-    
-    # iterate trough the grid
-    vertical = None
-    for x in range(len(board)):
-        for y in range(len(board)):
-            pass
 
+    winner = None
+    # iterate trough x,y in the board
+    for x in range(3):
+        for y in range(3):
 
+            # check first diagonal
+            if x == 1 and y == 1:
+                winner = board[x][y] if board[x-1][y-1] == board[x][y] == board[x+1][y+1] else None
 
+                # check second diagonal if no winner was found
+                if winner == None:
+                    winner = board[x][y] if board[x+1][y-1] == board[x][y] == board[x-1][y+1] else None
 
-    print(board)
+            # check horizontal lines
+            if x == 1 and winner == None:
+                winner = board[x][y] if board[x-1][y] == board[x][y] == board[x+1][y] else None
+
+            # check vertical lines
+            if y == 1 and winner == None:
+                winner = board[x][y] if board[x][y-1] == board[x][y] == board[x][y+1] else None
+
+            # return a winner if one is found
+            if winner:
+                return winner
+            
+            # previous if statements checked if a winner was found before to avoid
+            # overwritting a previously found winning line, with a line of EMPTY cells 
+
+    # return None if no winner was found        
     return None
 
 
